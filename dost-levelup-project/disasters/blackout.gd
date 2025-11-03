7cae4e533e92ba01f7e3338fc00197e3c527a778
AmeOnna-cell <linanthony554@gmail.com>
2025-11-03 10:08:14 +0800

    lols

--- dost-levelup-project/disasters/blackout.gd
+++ dost-levelup-project/disasters/blackout.gd
@@ -3,7 +3,9 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Play disaster sound if AudioStreamPlayer2D exists
+	if has_node("AudioStreamPlayer2D"):
+		$AudioStreamPlayer2D.play()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
